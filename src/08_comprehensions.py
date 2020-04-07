af8e846ff74c2f51e8afd67618b2ff193b8c03ed
Robbie Porter <rthomporter@gmail.com>
2020-04-06 20:13:32 -0500

    up to 10_functions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -2,7 +2,7 @@
 List comprehensions are one cool and unique feature of Python.
 They essentially act as a terse and concise way of initializing
 and populating a list given some expression that specifies how
-the list should be populated. 
+the list should be populated.
 
 Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
 for more info regarding list comprehensions.
@@ -12,12 +12,18 @@ for more info regarding list comprehensions.
 
 y = []
 
-print (y)
+for x in range(6):
+    y.append(x)
+
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
+
+for x in range(10):
+    y.append(x**3)
 
 print(y)
 
@@ -28,6 +34,9 @@ a = ["foo", "bar", "baz"]
 
 y = []
 
+for i in a:
+    y.append(i.upper())
+
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
@@ -37,5 +46,10 @@ x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
 y = []
+
+for i in x:
+    if int(i) % 2 == 0:
+        y.append(int(i))
+    pass
 
 print(y)
